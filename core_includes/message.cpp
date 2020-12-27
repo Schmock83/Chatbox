74dbfb39426c06ac6905ef12b846c454172e3723
Schmock83 <Schmock83@protonmail.com>
2020-12-27 16:51:40 +0100

    cleaned code x2

--- core_includes/message.cpp
+++ core_includes/message.cpp
@@ -1,5 +1,5 @@
 #include "message.h"
 
 Message::Message(QString name, int age, double money)
-    :name(name), age(age), money(money)
+	:name(name), age(age), money(money)
 {}
--- core_includes/message.h
+++ core_includes/message.h
@@ -8,22 +8,22 @@
 class Message
 {
 public:
-    Message(QString name = "Name", int age = 0, double money = 0.00);
-    friend QDataStream &operator<<(QDataStream &stream, const Message &message){
-        stream << message.name << message.age << message.money;
-        return stream;
-    }
-    friend QDataStream &operator>>(QDataStream &stream, Message &message){
-        stream >> message.name >> message.age >> message.money;
-        return stream;
-    }
-    void print()const{
-        qDebug() << name << " - " << age << " - " << money;
-    }
+	Message(QString name = "Name", int age = 0, double money = 0.00);
+	friend QDataStream& operator<<(QDataStream& stream, const Message& message) {
+		stream << message.name << message.age << message.money;
+		return stream;
+	}
+	friend QDataStream& operator>>(QDataStream& stream, Message& message) {
+		stream >> message.name >> message.age >> message.money;
+		return stream;
+	}
+	void print()const {
+		qDebug() << name << " - " << age << " - " << money;
+	}
 private:
-    QString name;
-    int age;
-    double money;
+	QString name;
+	int age;
+	double money;
 };
 
 #endif // MESSAGE_H
