24aae31bcb0a42f8acf7a7d994ffaf574995801b
Schmock83 <Schmock83@protonmail.com>
2020-12-29 13:23:54 +0100

    added library error message when sodium library wasnt loaded properly

--- Client_Chatbox/src/main.cpp
+++ Client_Chatbox/src/main.cpp
@@ -3,9 +3,18 @@
 #include <QApplication>
 #include <QMessageBox>
 
+#include <sodium.h>
+
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+
+	if (sodium_init() < 0) {
+		qDebug() << "HI";
+		QMessageBox::information(nullptr, "Library Error", "Sodium library could not be initialized properly!");
+		return -1;
+	}
+
 	MainWindow w;
 	w.show();
 	return a.exec();
