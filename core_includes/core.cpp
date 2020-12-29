a4b305956e7e684342177305a4f76b1401e95811
Schmock83 <Schmock83@protonmail.com>
2020-12-29 13:26:00 +0100

    cleaned code

--- core_includes/core.cpp
+++ core_includes/core.cpp
@@ -14,7 +14,6 @@ namespace UI {
 		}
 	}
 }
-
 namespace CRYPTO {
 	QString hashPassword(const QString& unhashed_password)//TODO error
 	{
@@ -23,17 +22,20 @@ namespace CRYPTO {
 
 		if (CRYPTO::hashPassword(hashed_password, unhashed_password_cstr))
 			return QString(hashed_password);
-		throw QString("Not enough memory to hash");
 	}
-    bool hashPassword(char* hashed_password, char* unhashed_password)
+	bool hashPassword(char* hashed_password, char* unhashed_password)
 	{
-		return (crypto_pwhash_str(hashed_password, unhashed_password, strlen(unhashed_password), crypto_pwhash_OPSLIMIT_MIN, crypto_pwhash_MEMLIMIT_MIN) == 0);
+		if (crypto_pwhash_str(hashed_password, unhashed_password, strlen(unhashed_password), crypto_pwhash_OPSLIMIT_SENSITIVE, crypto_pwhash_MEMLIMIT_SENSITIVE) != 0) {
+			//not enough memory -> try again with less memory usage
+			return (crypto_pwhash_str(hashed_password, unhashed_password, strlen(unhashed_password), crypto_pwhash_OPSLIMIT_MIN, crypto_pwhash_MEMLIMIT_MIN) == 0);
+		}
+		return true;
 	}
-    bool verifyPassword(const char* hashed_password, const char* password_to_verify)
+	bool verifyPassword(const char* hashed_password, const char* password_to_verify)
 	{
 		return (crypto_pwhash_str_verify(hashed_password, password_to_verify, strlen(password_to_verify)) == 0);
 	}
-    bool verifyPassword(const QString& hashed_password, const QString& password_to_verify)
+	bool verifyPassword(const QString& hashed_password, const QString& password_to_verify)
 	{
 		char* password_to_verify_cstr = HELPER::QStringToCString(password_to_verify);
 		char* hashed_password_cstr = HELPER::QStringToCString(hashed_password);
