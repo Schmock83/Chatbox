11291016e30f01ae55d739858176d076fad6c686
Schmock83 <Schmock83@protonmail.com>
2021-01-01 17:53:27 +0100

    added error handling

--- Server_Chatbox/src/chatbox_server.cpp
+++ Server_Chatbox/src/chatbox_server.cpp
@@ -1,27 +1,62 @@
 #include "chatbox_server.h"
 
-bool Chatbox_Server::startServer(const QHostAddress& address, quint16 port)
+void Chatbox_Server::startServer(const QHostAddress& address, quint16 port)
 {
+	//start database
+	try {
+		database->initiate_sqlite_database();
+	}
+	catch (QSqlError sql_error) {
+		if (sql_error.text().isEmpty())
+			throw Server_Error("Could not initialize database");
+		else
+			throw Server_Error("Could not initialize database: " + sql_error.text());
+	}
+
+	//start tcp-server
 	if (!tcp_server->listen(address, port)) {
-		throw Server_Error(tcp_server->errorString());
+		throw Server_Error("Could not start the tcp-server: " + tcp_server->errorString());
 	}
-	else {
-		connect(tcp_server, SIGNAL(newConnection()), this, SLOT(new_connection()));
-		return true;
-	}
+
+	connect(tcp_server, SIGNAL(newConnection()), this, SLOT(new_connection()));
 }
 
 void Chatbox_Server::new_connection()
 {
-	qDebug() << "new connection";
 	QTcpSocket* new_connection_socket = tcp_server->nextPendingConnection();
 	connect(new_connection_socket, SIGNAL(readyRead()), this, SLOT(new_data_in_socket()));
 }
 
 void Chatbox_Server::new_data_in_socket()
 {
-	qDebug() << "received new Data";
 	QTcpSocket* clientSocket = (QTcpSocket*)sender();
 	Message msg = Message::readFromSocket(clientSocket);
-	msg.print();
+	QThread* thread = QThread::create([&, msg, clientSocket] { handleMessage(msg, clientSocket); });
+	thread->start();
+}
+
+void Chatbox_Server::handleMessage(const Message& message, QTcpSocket* client_socket)
+{
+	switch (message.getMessageType()) {
+	case MessageType::registrationMessage:
+		handleRegistration(message, client_socket);
+		break;
+	case MessageType::loginMessage:
+		break;
+	}
+}
+
+void Chatbox_Server::handleRegistration(const Message& message, QTcpSocket* client_socket)
+{
+	try {
+		if (database->user_registered(message.getSender())) {
+			//TODO send back message -> user with that name already exists....
+			return;
+		}
+		database->register_user(message.getSender(), message.getContent());
+		//success -> TODO: send back successfully registered ...
+	}
+	catch (QSqlError error) {
+		qDebug() << "Error in handleRegistration: " << error.text();
+	}
 }
--- Server_Chatbox/src/chatbox_server.h
+++ Server_Chatbox/src/chatbox_server.h
@@ -6,8 +6,10 @@
 #include <QTcpSocket>
 
 #include <QHash>
+#include <QThread>
 #include "user.h"
 #include "../core_includes/message.h"
+#include "databasehelper.h"
 
 #include <QDebug>
 
@@ -26,13 +28,18 @@ class Chatbox_Server : public QWidget
 	Q_OBJECT
 private:
 	QTcpServer* tcp_server = nullptr;
-	QHash<QString, User*> authenticated_online_users;
+	DatabaseHelper* database = nullptr;
+	//QHash<QString, User*> authenticated_online_users;
+
+	void handleMessage(const Message& message, QTcpSocket* client_socket);
+	void handleRegistration(const Message& message, QTcpSocket* client_socket);
 public:
 	Chatbox_Server(QWidget* parent = nullptr)
 		:QWidget(parent)
 		, tcp_server(new QTcpServer())
+		, database(new DatabaseHelper())
 	{}
-	bool startServer(const QHostAddress& address = QHostAddress::Any, quint16 port = 0);
+	void startServer(const QHostAddress& address = QHostAddress::Any, quint16 port = 0);
 private slots:
 	void new_data_in_socket();
 	void new_connection();
