ca97a27e4d2f3ab5dc8f0ec6c08c28c72a9d0321
Schmock83 <Schmock83@protonmail.com>
2021-01-01 11:39:30 +0100

    added user-class, to represent a user in the server

--- Server_Chatbox/src/user.cpp
+++ Server_Chatbox/src/user.cpp
@@ -1,5 +1,1 @@
 #include "user.h"
-
-User::User()
-{
-}
--- Server_Chatbox/src/user.h
+++ Server_Chatbox/src/user.h
@@ -1,10 +1,41 @@
 #ifndef USER_H
 #define USER_H
 
+#include <QString>
+#include <QDateTime>
+#include <QTcpSocket>
+
+#include "databasehelper.h"
+
 class User
 {
+private:
+	int user_id;
+	QString user_name;
+	QDateTime registry_date;
+	QDateTime last_login;
+	QTcpSocket* tcp_socket;
 public:
-	User();
+	User(int user_id, QString user_name, QDateTime registry_date, QDateTime last_login = QDateTime(), QTcpSocket* socket = nullptr)
+		:user_id(user_id), user_name(user_name), registry_date(registry_date), last_login(last_login), tcp_socket(socket)
+	{}
+	User(int user_id, QString user_name, QDateTime registry_date, QTcpSocket* socket = nullptr, QDateTime last_login = QDateTime())
+		:user_id(user_id), user_name(user_name), registry_date(registry_date), last_login(last_login), tcp_socket(socket)
+	{}
+
+	void set_tcp_socket(QTcpSocket* tcp_socket) {
+		tcp_socket = tcp_socket;
+	}
+
+	int get_user_id()const { return user_id; }
+	QString get_user_name()const { return user_name; }
+	QDateTime get_registry_date() const { return registry_date; }
+	QDateTime get_last_login()const { return last_login; }
+	QTcpSocket* get_tcp_socket()const { return tcp_socket; }
+
+	bool user_never_logged_in()const { return last_login.isValid(); }
+	bool user_connected()const { return (tcp_socket != nullptr && tcp_socket->isValid()); }
+	bool user_online()const { return user_connected(); }
 };
 
 #endif // USER_H
