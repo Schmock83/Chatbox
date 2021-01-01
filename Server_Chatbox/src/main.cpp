2c0cecd350ac1fca4aef5583d8b8a44dcb9ea2b4
Schmock83 <Schmock83@protonmail.com>
2021-01-01 11:39:03 +0100

    implemented check if QTcpServer was started in main

--- Server_Chatbox/src/main.cpp
+++ Server_Chatbox/src/main.cpp
@@ -3,10 +3,27 @@
 #include <QApplication>
 #include <QMessageBox>
 
+#include <sodium.h>
+
+#include "databasehelper.h"
+#include <QHash>
+#include "user.h"
+
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+
+	//check if libsodium was initialized properly
+	if (sodium_init() < 0) {
+		QMessageBox::information(nullptr, "Library Error", "Sodium library could not be initialized properly!");
+		return -1;
+	}
+
 	MainWindow w;
+	//check if server was started properly:
+	if (!w.initialize_server()) {
+		return -2;
+	}
 	w.show();
 	return a.exec();
 }
