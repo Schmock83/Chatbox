9ddf2b0abb7b90690a6308098b81da3a64287980
Schmock83 <Schmock83@protonmail.com>
2021-01-02 18:00:36 +0100

    added database support for user class

--- Server_Chatbox/src/user.cpp
+++ Server_Chatbox/src/user.cpp
@@ -1,1 +1,17 @@
 #include "user.h"
+
+User::User(QString user_name, DatabaseHelper* database, QTcpSocket* socket)
+	: user_name(user_name)
+	, database(database)
+	, tcp_socket(socket)
+{
+	//check if user exists-> if not throw exception
+	if (!database->construct_user(user_name, user_id, registry_date, last_login))
+		throw NoUserInDatabase();
+}
+
+void User::update_last_login()
+{
+	if (database != nullptr && database->connected())
+		database->update_last_login(user_name, QDateTime::currentDateTime());
+}
--- Server_Chatbox/src/user.h
+++ Server_Chatbox/src/user.h
@@ -5,7 +5,14 @@
 #include <QDateTime>
 #include <QTcpSocket>
 
+#include <QDebug>
+
 #include "databasehelper.h"
+
+class NoUserInDatabase {
+public:
+	NoUserInDatabase() {}
+};
 
 class User
 {
@@ -15,6 +22,8 @@ private:
 	QDateTime registry_date;
 	QDateTime last_login;
 	QTcpSocket* tcp_socket;
+
+	DatabaseHelper* database = nullptr;
 public:
 	User(int user_id, QString user_name, QDateTime registry_date, QDateTime last_login = QDateTime(), QTcpSocket* socket = nullptr)
 		:user_id(user_id), user_name(user_name), registry_date(registry_date), last_login(last_login), tcp_socket(socket)
@@ -23,8 +32,14 @@ public:
 		:user_id(user_id), user_name(user_name), registry_date(registry_date), last_login(last_login), tcp_socket(socket)
 	{}
 
+	User(QString user_name, DatabaseHelper* database, QTcpSocket* socket = nullptr);
+
 	void set_tcp_socket(QTcpSocket* tcp_socket) {
 		tcp_socket = tcp_socket;
+	}
+
+	void set_database(DatabaseHelper* database) {
+		database = database;
 	}
 
 	int get_user_id()const { return user_id; }
@@ -34,8 +49,12 @@ public:
 	QTcpSocket* get_tcp_socket()const { return tcp_socket; }
 
 	bool user_never_logged_in()const { return last_login.isValid(); }
-	bool user_connected()const { return (tcp_socket != nullptr && tcp_socket->isValid()); }
-	bool user_online()const { return user_connected(); }
+	bool connected()const { return (tcp_socket != nullptr && tcp_socket->isValid() && tcp_socket->state() == QTcpSocket::ConnectedState); }
+	bool online()const { return connected(); }
+
+	void update_last_login();
+
+	void print()const { qDebug() << user_id << " - " << user_name << " - " << registry_date.toString() << " - " << last_login.toString(); }
 };
 
 #endif // USER_H
