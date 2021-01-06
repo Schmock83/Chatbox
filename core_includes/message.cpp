33c5375cf277a166b0c4f709ecbdfeb57c45962a
Schmock83 <Schmock83@protonmail.com>
2021-01-06 13:46:52 +0100

    adding Message-functionality to support searchUser and general Messages for server queries + results

--- core_includes/message.cpp
+++ core_includes/message.cpp
@@ -17,27 +17,51 @@ Message Message::createServerMessage(MessageType messageType, QString content)
 {
 	return Message(messageType, QDateTime::currentDateTime(), "", content);
 }
+Message Message::createServerMessage(MessageType messageType, QList<QString> str_list)
+{
+	return Message(messageType, QDateTime::currentDateTime(), "", str_list);
+}
 Message Message::createDefaultMessage(QDateTime dateTime, QString sender, QString receiver, QString content)
 {
 	return Message(MessageType::client_chatMessage, dateTime, sender, content, receiver);
 }
+Message Message::createClientRequstMessage(MessageType messageType, QString content)
+{
+	return Message(messageType, QDateTime::currentDateTime(), "", content);
+}
 
-Message Message::readFromStream(QDataStream& stream) {
+Message Message::readFromStream(QDataStream& stream)
+{
 	MessageType messageType;
 	QDateTime dateTime;
 	QString messageContents;
 	QString sender;
 	QString receiver;
+	QList<QString> str_list;
 
-	stream >> messageType >> dateTime >> sender >> receiver >> messageContents;
-
-	return Message(messageType, dateTime, sender, messageContents, receiver);
+	stream >> messageType >> dateTime >> sender;
+	if (messageType == MessageType::server_searchUserResult) {
+		stream >> str_list;
+		return Message(messageType, dateTime, sender, str_list);
+	}
+	else {
+		stream >> receiver >> messageContents;
+		return Message(messageType, dateTime, sender, messageContents, receiver);
+	}
 }
-QDataStream& Message::writeToStream(QDataStream& stream, const Message& message) {
-	stream << message.messageType << message.dateTime << message.sender << message.receiver << message.messageContents;
+QDataStream& Message::writeToStream(QDataStream& stream, const Message& message)
+{
+	stream << message.messageType << message.dateTime << message.sender;
+	if (message.messageType == MessageType::server_searchUserResult) {
+		stream << message.str_list;
+	}
+	else {
+		stream << message.receiver << message.messageContents;
+	}
 	return stream;
 }
-Message Message::readFromSocket(QTcpSocket* socket) {
+Message Message::readFromSocket(QTcpSocket* socket)
+{
 	//check if socket readable
 	if (!socket->isValid())
 		emit socket->disconnected();
@@ -46,7 +70,8 @@ Message Message::readFromSocket(QTcpSocket* socket) {
 	return Message::readFromStream(in);
 }
 
-void Message::sendThroughSocket(QTcpSocket* socket, const Message& message) {
+void Message::sendThroughSocket(QTcpSocket* socket, const Message& message)
+{
 	//check if socket readable
 	if (!socket->isValid())
 		emit socket->disconnected();
--- core_includes/message.h
+++ core_includes/message.h
@@ -4,6 +4,7 @@
 #include <QDataStream>
 #include <QTcpSocket>
 #include <QDateTime>
+#include <QList>
 
 #include <QDebug>
 
@@ -13,10 +14,12 @@ enum MessageType {
 	client_chatMessage = 0,
 	client_loginMessage,
 	client_registrationMessage,
+	client_searchUserRequest,
 	server_loginSucceeded,
 	server_loginFailed,
 	server_registrationSucceeded,
-	server_registrationFailed
+	server_registrationFailed,
+	server_searchUserResult
 };
 
 class Message
@@ -27,9 +30,13 @@ private:
 	QString messageContents;
 	QString sender;
 	QString receiver;
+	QList<QString> str_list;
 
 	Message(MessageType messageType, QDateTime dateTime, QString sender, QString content = "", QString receiver = "")
 		:messageType(messageType), dateTime(dateTime), messageContents(content), sender(sender), receiver(receiver)
+	{}
+	Message(MessageType messageType, QDateTime dateTime, QString sender, QList<QString> str_list, QString content = "", QString receiver = "")
+		:messageType(messageType), dateTime(dateTime), messageContents(content), sender(sender), receiver(receiver), str_list(str_list)
 	{}
 
 public:
@@ -37,6 +44,8 @@ public:
 	static Message createRegistrationMessage(QDateTime dateTime, QString username, QString unhashed_password);
 	static Message createDefaultMessage(QDateTime dateTime, QString sender, QString receiver, QString content);
 	static Message createServerMessage(MessageType messageType, QString content);
+	static Message createServerMessage(MessageType messageType, QList<QString> str_list);
+	static Message createClientRequstMessage(MessageType messageType, QString content);
 
 	static Message readFromStream(QDataStream& stream);
 	static Message readFromSocket(QTcpSocket* socket);
@@ -48,9 +57,10 @@ public:
 	QString getReceiver()const { return receiver; }
 	QDateTime getDateTime()const { return dateTime; }
 	QString getContent()const { return messageContents; }
+	QList<QString> getStringList()const { return str_list; }
 
 	void print()const {
-		qDebug() << messageType << dateTime << sender << messageContents << receiver;
+		qDebug() << messageType << dateTime << sender << messageContents << receiver << str_list;
 	}
 };
 
